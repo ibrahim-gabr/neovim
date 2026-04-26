1975c78a6872728e0b3420d2c042e642b2e8b37a
Ibrahim Abdallah <ibrahim.G@salla.sa>
2026-04-26 16:15:10 +0300

    add autosave and disable autoformat files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local autosave_group = vim.api.nvim_create_augroup("user_auto_save", { clear = true })
+
+local function auto_save(bufnr)
+  if not vim.api.nvim_buf_is_valid(bufnr) then
+    return
+  end
+
+  local bo = vim.bo[bufnr]
+  if bo.buftype ~= "" or not bo.modifiable or bo.readonly or not bo.modified then
+    return
+  end
+
+  if vim.api.nvim_buf_get_name(bufnr) == "" then
+    return
+  end
+
+  vim.api.nvim_buf_call(bufnr, function()
+    vim.cmd("silent update")
+  end)
+end
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertLeave" }, {
+  group = autosave_group,
+  callback = function(event)
+    auto_save(event.buf)
+  end,
+})
---
+++ lua/config/user.lua
@@ -0,0 +1,1 @@
+EcoVim.plugins.projects.dev = { "~/Salla/new_dashboard/ui-merchant-orders", "~/work/twilight" , "~/dev/projects/knowledge-base" }
